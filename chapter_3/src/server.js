a64ca06475b49196e99b16ff222e92b10cdf18e3
Ritpra93 <ritpra93@gmail.com>
2025-06-08 12:28:08 -0500

    backend working

--- chapter_3/src/server.js
+++ chapter_3/src/server.js
@@ -1,22 +1,29 @@
 import express from 'express'
 import path, { dirname } from 'path'
-import {fileURLToPath} from 'url'
+import { fileURLToPath } from 'url'
 
 
 const app = express()
 const PORT = process.env.PORT || 5003
 
-//Get the file path from the URL of the current module
+// Get the file path from the URL of the current module
 const __filename = fileURLToPath(import.meta.url)
-//Get the directory name from the file path
-const __dirname = dirname(__filename);
+// Get the directory name from the file path
+const __dirname = dirname(__filename)
 
-app.use(express.static[path.join(__dirname, '../public')])
-//serving up the HTML frile from the /public directory 
-app.get('/', (req, res) =>{
-    res.sendFile(path.join(__dirname,  'public', 'index.html'))
+// Middleware
+app.use(express.json())
+// Serves the HTML file from the /public directory
+// Tells express to serve all files from the public folder as static assets / file. Any requests for the css files will be resolved to the public directory.
+app.use(express.static(path.join(__dirname, '../public')))
+
+// Serving up the HTML file from the /public directory
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
+
+
 app.listen(PORT, () => {
-    console.log(`Server has started on port ${PORT}`)
+    console.log(`Server has started on port: ${PORT}`)
 })
