b9c79d28ffa00044ce0c46b4d8c1e4c782303d29
Ritpra93 <ritpra93@gmail.com>
2025-06-11 23:59:16 -0500

    chapter 3 done i think

--- chapter_3/src/routes/todoRoutes.js
+++ chapter_3/src/routes/todoRoutes.js
@@ -21,11 +21,27 @@ router.post('/', (req,res) => {
 
 //update todo
 router.put('/:id', (req,res) =>{
+    const {completed} = req.body
+    const {id} = req.params
+    const {page} = req.query
+
+    const updatedTodo = db.prepare(`UPDATE todos SET task completed = ? WHERE id = ?
+        `)
+
+        updatedTodo.run(completed, id)
+
+        res.json({message: "Todo completed"})
 
 })
 
 //delete a todo
 router.delete('/:id', (req,res) =>{
+    const { id } = req.params
+    const userId = req.userId
+    const deleteTodo = db.prepare(`DELETE FROM todos WHERE id = ? AND user_id = ?
+        `)
+
+    deleteTodo.run(id, userId)
 
 })
 
